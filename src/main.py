cc7edd041360daf2efd32551fe6e3ee9c7e4b048
Senay <senaykarisful@gmail.com>
2022-05-20 02:08:12 +0400

    made minor modifications

--- src/main.py
+++ src/main.py
@@ -7,59 +7,38 @@ import pandas as pd
 from utils import *
 from models import *
 
+from train_detector import *
+from ML_training import *
+
 def main ():
 
-    data_loader = database_creation(lifetime = 287) # You can change lifetime here
 
-    # -------Training Models using Sensor data only -------------
+    # -------Training our traffic event detector with MLP Model  -------------
 
-    # Definning our trarget variables and predictors
-    TargetVariable=['Class']
-    Predictors=['SensorsID', 'Speed', 'Feature1', 'Feature2', 'Feature3', 'Feature4',
-                'Feature5', 'Feature6'] #'tweet_lifetime', 'tweet_binary'
+    # Using Sensor data only
+    train_detector (model = "MLP" , with_tweets = False)
 
+    # Using Sensor and Twitter data
+    train_detector (model = "MLP" , with_tweets = True)
 
-    X = data_loader[Predictors].values  # Sensors data only
-    y = data_loader[TargetVariable].values
+    # -------Training our traffic event detector with CNN Model  -------------
 
-    # Standardizing the values of X
-    X, PredictorScalerFit = z_score(X) # Z-score normalization
-
-    # Split the data into training and testing set
-    xTrain, xTest, yTrain, yTest = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    # Creating the MLP model
-    MLP_model = mlp_model(8)
-
-    # Training/Fitting the MLP model
-    training_history = MLP_model.fit(xTrain,xTest, batch_size=16 , epochs=200, verbose=1)
-
-    # Plotting the training results
-    plot_training_results(training_history)
-
-    # Testing the our training model and generating prediction
-    Predictions=MLP_model.predict(xTest)
-    
-    # Scaling the test data back to original scale
-    Test_Data=PredictorScalerFit.inverse_transform(xTest)
-    
-    # Generating a data frame for analyzing the test data
-    TestingData=pd.DataFrame(data=Test_Data, columns=Predictors)
-    TestingData['TrafficEvent']=yTest
-    TestingData['PredictedEventProb']=Predictions
-
-    # Generating predictions on the testing data by applying probability threshold
-    TestingData['PredictedTrafficEvent']=TestingData['PredictedEventProb'].apply(probThreshold)
- 
-    print('\n######### Testing Accuracy Results - Sensor #########')
-    print(metrics.classification_report(TestingData['TrafficEvent'], TestingData['PredictedTrafficEvent'], digits = 4))
-    print(metrics.confusion_matrix(TestingData['TrafficEvent'], TestingData['PredictedTrafficEvent']))
-    matrix_confusion = confusion_matrix(TestingData['TrafficEvent'], TestingData['PredictedTrafficEvent'])
-    sns.heatmap(matrix_confusion, square=True, annot=True, cmap='Blues', fmt='d', cbar=False)
-
-    Predictors_twt = [ 'SensorsID', 'Speed', 'Feature1', 'Feature2', 'Feature3', 'Feature4',
-                'Feature5', 'Feature6', 'tweet_lifetime', 'tweet_binary'] #'tweet_lifetime', 'tweet_binary'
+    # Using Sensor and Twitter data
+    train_detector (model = "CNN" , with_tweets = True)
 
 
+    # -------Training our traffic event detector with MLL Model  -------------
+    ''''
+     If you want to train the model with MLL Model, you need to uncomment the following lines
+    '''
+    # remove_sensor = False
+    # remove_description = True
+    # sample = 'Oversampling'
+    # train_knn(remove_description,remove_sensor,sample)
+    # train_decisionTree(remove_description,remove_sensor,sample)
+    # train_RandomForest(remove_description,remove_sensor,sample)
+    # train_GBS(remove_description,remove_sensor,sample)
+    # train_XGB(remove_description,remove_sensor,sample)
 
-
+if __name__ == "__main__":
+    main()
--- src/models.py
+++ src/models.py
@@ -8,20 +8,23 @@ from keras.layers.convolutional import MaxPooling1D
 def mlp_model(input_size):
     # create model
     model = Sequential()
-    model.add(Dense(64, input_dim=input, activation='relu'))
+    model.add(Dense(64, input_dim=input_size, activation='relu'))
     model.add(Dense(32, activation='relu'))
     model.add(Dense(1, activation = 'sigmoid'))
     # Compile model
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
-def cnn_model ():
+def cnn_model (feture_size):
+    num_outputs = 1
     model = Sequential()
-    model.add(Conv1D(filters=64, kernel_size=2, activation='relu', input_shape=( 1,10), padding = 'same'))
+    model.add(Conv1D(filters=64, kernel_size=2, activation='relu', input_shape=( 1,feture_size), padding = 'same'))
     model.add(Conv1D(filters=64, kernel_size=2, activation='relu', padding = 'same'))
 
     model.add(MaxPooling1D(pool_size=2 , padding = 'same'))
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
-    model.add(Dense(n_outputs, activation='sigmoid'))
+    model.add(Dense(num_outputs, activation='sigmoid'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    
+    return model
