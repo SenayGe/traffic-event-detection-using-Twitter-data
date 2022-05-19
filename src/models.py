9685e426d5862118bf20d7b6c91c9d4f9393956f
Senay <senaykarisful@gmail.com>
2022-05-19 19:27:07 +0400

    added models

--- src/models.py
+++ src/models.py
@@ -1,5 +1,11 @@
-# define baseline model
-def baseline_model(input):
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers import Dropout
+from keras.layers.convolutional import Conv1D, Conv2D
+from keras.layers.convolutional import MaxPooling1D
+# define models
+def mlp_model(input_size):
     # create model
     model = Sequential()
     model.add(Dense(64, input_dim=input, activation='relu'))
@@ -8,3 +14,14 @@ def baseline_model(input):
     # Compile model
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
+
+def cnn_model ():
+    model = Sequential()
+    model.add(Conv1D(filters=64, kernel_size=2, activation='relu', input_shape=( 1,10), padding = 'same'))
+    model.add(Conv1D(filters=64, kernel_size=2, activation='relu', padding = 'same'))
+
+    model.add(MaxPooling1D(pool_size=2 , padding = 'same'))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(n_outputs, activation='sigmoid'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
